30487df8bcd713fca0e7600a6d12da35485947f7
Moonllighter <89983354+Moonllighter@users.noreply.github.com>
2021-11-09 15:41:50 +0200

    Update homework2_Ex3.rb

--- homework2_Ex3.rb
+++ homework2_Ex3.rb
@@ -2,5 +2,5 @@ puts 'Enter your favorite cities'
 like = gets.chomp
 puts 'Enter the cities you hate the most'
 unlike = gets.chomp
-result = like.squeeze(" ").split(', ').sort + unlike.squeeze(" ").split(', ').sort
-puts result.join(", ")
+result = like.split(',').sort + unlike.split(',').sort
+puts result.map{|i| i.strip}.join(", ")
